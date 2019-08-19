71fcbce4514505dbf116167b8b46de0a9b688629
Emil Weigelt Kjær Nielsen <emil601b@edu.campusvejle.dk>
2019-08-19 02:10:32 +0200

    Add project files.

---
+++ WPF Opgave 1/BIZ/ClassBIZ.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WPF_Opgave_1
+{
+    class ClassBIZ
+    {
+        public ClassBIZ()
+        {
+
+        }
+        public void Opgave12(ListBox listBox)
+        {
+            listBox.Items.Clear();
+
+            int start = 4711;
+            while (start < 4736)
+            {                
+                listBox.Items.Add(start.ToString());
+                start++;
+            }
+        }
+        public void Opgave13(ListBox listBox)
+        {
+            Random random = new Random();
+            listBox.Items.Clear();
+
+            for(int i = 0; i < 25; i++)
+            {
+                int randomTal = random.Next(100000,1000001);
+                listBox.Items.Add(randomTal.ToString());
+            }
+        }
+        public void Opgave14(ListBox listBox)
+        {
+            Random random = new Random();
+            List<int> listInt = new List<int>();
+
+            listBox.Items.Clear();
+
+            for (int i = 0; i < 25; i++)
+            {
+                int randomTal = random.Next(100000, 1000001);
+                listInt.Add(randomTal);
+            }
+
+            listInt.Sort();
+
+            foreach(int tal in listInt)
+            {
+                listBox.Items.Add(tal.ToString());
+            }
+        }
+    }
+}
---
+++ WPF Opgave 1/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Opgave_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ClassBIZ classBIZ = new ClassBIZ();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_0_Click(object sender, RoutedEventArgs e)
+        {
+            classBIZ.Opgave12(ListBoxRes);
+        }
+
+        private void Button_1_Click(object sender, RoutedEventArgs e)
+        {
+            classBIZ.Opgave13(ListBoxRes);
+        }
+
+        private void Button_2_Click(object sender, RoutedEventArgs e)
+        {
+            classBIZ.Opgave14(ListBoxRes);
+        }
+    }
+}
